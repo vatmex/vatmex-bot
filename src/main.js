4988ef535d68a1df86060628089815b0c82a35dd
Ethan Mahlstedt <76834762+ImSpeddy@users.noreply.github.com>
2023-07-01 16:19:44 -0700

    Correción en la acentuación, cambio de formato en embeds
    * Corrige la acentuación de airports.json
    * Modifica el formato de los embeds

--- src/main.js
+++ src/main.js
@@ -38,7 +38,7 @@ async function checkControllers() {
 	const newControllersOnline = {};
 
 	for (const controller of data.controllers) {
-		if (controller.callsign.startsWith('MM')) {
+		if (controller.callsign.startsWith('MM') && controller.facility != 0 && controller.facility != 1 && controller.rating < 11 && controller.rating > 1) {
 			newControllersOnline[controller.callsign] = true;
 		  	
 			// This controller just came online.
@@ -51,8 +51,7 @@ async function checkControllers() {
 					.setColor("13437C")
 					.setTitle(`${position} en linea!`)
 					.setURL(`https://stats.vatsim.net/stats/${controller.cid}`)
-					.setDescription(`${controller.name} [${controller.cid}] se ha conectado en ${position} [${controller.callsign}]!`)
-					.setThumbnail('https://cdn.discordapp.com/attachments/803128345930760195/1124765846988927038/Imagotipo-01.png')
+					.setDescription(`\`\`\`js\n${controller.name} [${controller.cid}] se ha conectado en \"${position}\" [${controller.callsign}]!\`\`\``)
 					.setTimestamp(Date.now())
 
 				client.channels.cache.get(process.env.ACTIVITY_CHANNEL_ID).send({embeds: [newControllerEmbed]});
@@ -73,8 +72,6 @@ async function checkControllers() {
 			const offlineControllerEmbed = new EmbedBuilder()
 			.setColor("13437C")
 			.setTitle(`${position} se ha desconectado.`)
-			.setDescription(`${position} [${controllerCallsign}] se ha desconectado`)
-			.setThumbnail('https://cdn.discordapp.com/attachments/803128345930760195/1124765846988927038/Imagotipo-01.png')
 			.setTimestamp(Date.now())
 			
 		  	client.channels.cache.get(process.env.ACTIVITY_CHANNEL_ID).send({embeds: [offlineControllerEmbed]});
@@ -92,7 +89,7 @@ async function listControllers(interaction) {
 	let activeControllers = [];
 	for (const controller of data.controllers) 
 	{
-		if (controller.callsign.startsWith('MM'))
+		if (controller.callsign.startsWith('MM') && controller.facility != 0 && controller.facility != 1 && controller.rating < 11 && controller.rating > 1)
 		{
 			activeControllers.push(
 				{
@@ -114,14 +111,13 @@ async function listControllers(interaction) {
 		const controllerListEmbed = new EmbedBuilder()
 		.setColor("13437C")
 		.setTitle(`Lista de controladores:`)
-		.setThumbnail('https://cdn.discordapp.com/attachments/803128345930760195/1124765846988927038/Imagotipo-01.png')
 		.setTimestamp(Date.now())
 
 		for (var i = 0; i < activeControllers.length; i++) 
 		{
 			let position = callsignToText(activeControllers[i].callsign);
 
-			controllerListEmbed.addFields({ name: position, value: `${position} [${activeControllers[i].callsign}] : ${activeControllers[i].name} [${activeControllers[i].cid}]` })
+			controllerListEmbed.addFields({ name: position, value: `\`\`\`js\n\"${position}\" [${activeControllers[i].callsign}] : ${activeControllers[i].name} [${activeControllers[i].cid}]\`\`\`` })
 		}
 
 		interaction.reply({embeds: [controllerListEmbed]});
