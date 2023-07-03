e964a2b5aa5bb731767b3c42080d5ebf9b6cfd21
Gustavo Valdez <gvaldezsan@gmail.com>
2023-07-03 05:16:54 -0600

    Corregido a estándares Wesbos

--- src/register-commands.js
+++ src/register-commands.js
@@ -1,26 +1,27 @@
 require('dotenv').config();
 
-const {REST, Routes} = require('discord.js');
+const { REST, Routes } = require('discord.js');
 
 const commands = [
-    {
-        name: 'cta',
-        description: 'Muestra una lista de los controladores de la división conectados a la red',
-    },
+  {
+    name: 'cta',
+    description:
+      'Muestra una lista de los controladores de la división conectados a la red',
+  },
 ];
 
-const rest = new REST({ version: '10'}).setToken(process.env.TOKEN);
+const rest = new REST({ version: '10' }).setToken(process.env.TOKEN);
 
 (async () => {
-    try 
-    {
-        await rest.put(
-            Routes.applicationGuildCommands(process.env.CLIENT_ID, process.env.GUILD_ID),
-            { body: commands }
-        );
-    } 
-    catch (error) 
-    {
-        console.log(`${new Date().toISOString()} - Error: ${error}`);
-    }
+  try {
+    await rest.put(
+      Routes.applicationGuildCommands(
+        process.env.CLIENT_ID,
+        process.env.GUILD_ID
+      ),
+      { body: commands }
+    );
+  } catch (error) {
+    console.log(`${new Date().toISOString()} - Error: ${error}`);
+  }
 })();
