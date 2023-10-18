7e31c4319dcfce1fdaee8187914a5adc9a8708ee
Gustavo Valdez <gvaldezsan@gmail.com>
2023-10-18 10:51:16 -0600

    Adds Sentry

--- src/main.js
+++ src/main.js
@@ -1,14 +1,33 @@
 require('dotenv').config();
 
 const { Client, IntentsBitField, EmbedBuilder } = require('discord.js');
-// eslint-disable-next-line import/no-extraneous-dependencies
 const express = require('express');
+const Sentry = require('@sentry/node');
+const { ProfilingIntegration } = require('@sentry/profiling-node');
 const station = require('./airports.json');
 const unauthorizedController = require('./functions/unauthorizedController');
 const API = require('./functions/api');
 
 const app = express();
+
+Sentry.init({
+  dsn: process.env.SENTRY_DSN,
+  integrations: [
+    // enable HTTP calls tracing
+    new Sentry.Integrations.Http({ tracing: true }),
+    // enable Express.js middleware tracing
+    new Sentry.Integrations.Express({ app }),
+    new ProfilingIntegration(),
+  ],
+  // Performance Monitoring
+  tracesSampleRate: 1.0,
+  // Set sampling rate for profiling - this is relative to tracesSampleRate
+  profilesSampleRate: 1.0,
+});
+
 app.use(express.json());
+app.use(Sentry.Handlers.requestHandler());
+app.use(Sentry.Handlers.tracingHandler());
 
 // Store the online controllers
 let controllersOnline = {};
@@ -191,6 +210,15 @@ app.post('/application', API.authenticateKey, (req, res) => {
     .send({ embeds: [newApplicationEmbed] });
 
   res.send('Evento noticiado con exito!');
+});
+
+app.use(Sentry.Handlers.errorHandler());
+
+app.use((err, req, res, next) => {
+  // The error id is attached to `res.sentry` to be returned
+  // and optionally displayed to the user for support.
+  res.statusCode = 500;
+  res.end(`${res.sentry}\n`);
 });
 
 app.listen(8090, () => {
