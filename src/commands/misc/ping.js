5692deb0b7327062380242a70a258f2f23e17b02
Gustavo Valdez <gvaldezsan@gmail.com>
2023-11-13 16:09:47 -0600

    Updates metar command to new architecture

--- src/commands/misc/ping.js
+++ src/commands/misc/ping.js
@@ -1,7 +1,9 @@
+const { PermissionFlagsBits } = require('discord.js');
+
 module.exports = {
   name: 'ping',
   description: 'Revisa si el Bot sigue vivo',
-
+  permissionsRequired: [PermissionFlagsBits.Administrator],
   callback: (bot, interaction) => {
     interaction.reply('¡Aún sigo vivo!');
   },
