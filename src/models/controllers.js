80d66051a9e754133f7bc7a1273065e0c3630fda
Gustavo Valdez <gvaldezsan@gmail.com>
2023-11-14 02:45:50 -0600

    Updates Vatsim request logic with axios

--- src/models/controllers.js
+++ src/models/controllers.js
@@ -3,44 +3,45 @@ const axios = require('axios');
 let controllersOnline = {};
 
 const updateControllers = async (bot, callback) => {
-  const response = await fetch(process.env.VATSIM_DATA_URL);
-  const data = await response.json();
-
   const request = {
     method: 'get',
     url: process.env.VATSIM_DATA_URL,
   };
 
-  axios(request).then(async (axiosresponse) => {
-    console.log(axiosresponse.data);
-  });
+  axios(request)
+    .then(async (response) => {
+      const newControllersOnline = {};
 
-  const newControllersOnline = {};
+      for (const controller of response.data.controllers) {
+        const splitCallsign = controller.callsign.split('_');
 
-  for (const controller of data.controllers) {
-    const splitCallsign = controller.callsign.split('_');
+        if (
+          controller.callsign.startsWith('MM') && // Only México
+          controller.facility !== 0 && // Hide observers
+          splitCallsign[0].length === 4 && // Hide non standard positions
+          splitCallsign[1] !== 'I' // Hide instructors
+        ) {
+          const newController = {
+            online: true,
+            cid: controller.cid,
+            name: controller.name,
+          };
+          newControllersOnline[controller.callsign] = newController;
 
-    if (
-      controller.callsign.startsWith('MM') && // Only México
-      controller.facility !== 0 && // Hide observers
-      splitCallsign[0].length === 4 && // Hide non standard positions
-      splitCallsign[1] !== 'I' // Hide instructors
-    ) {
-      const newController = {
-        online: true,
-        cid: controller.cid,
-        name: controller.name,
-      };
-      newControllersOnline[controller.callsign] = newController;
-
-      if (!controllersOnline[controller.callsign]) {
-        // This controller just came online
-        callback(controller, controller.callsign);
+          if (!controllersOnline[controller.callsign]) {
+            // This controller just came online
+            callback(controller, controller.callsign);
+          }
+        }
       }
-    }
-  }
 
-  controllersOnline = newControllersOnline;
+      controllersOnline = newControllersOnline;
+    })
+    .catch((error) => {
+      console.log(
+        `${new Date().toISOString()} - ERROR: Failed to fetch controller data from Vatsim.`
+      );
+    });
 };
 
 const getOnlineControllers = () => controllersOnline;
